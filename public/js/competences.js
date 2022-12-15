a1e6e15f2312d489541d54e40a537f3baed8f192
Hubrec <92845931+Hubrec@users.noreply.github.com>
2022-12-15 12:03:46 +0100

    competences page

--- public/js/competences.js
+++ public/js/competences.js
@@ -1,6 +1,7 @@
 const pageCompetences = document.querySelector('.myCompetences');
 const content = pageCompetences.querySelector('.content');
 const selecting = content.querySelectorAll('li');
+const sections = content.querySelectorAll('section');
 
 selecting.forEach((item) => {
     item.addEventListener('click', (e) => {
@@ -8,5 +9,18 @@ selecting.forEach((item) => {
             item.classList.remove('selected');
         });
         e.target.classList.add('selected');
+        showSection();
     });
 });
+
+function showSection() {
+    sections.forEach((item) => { item.remove() });
+
+    for (let i = 0; i < sections.length; i++) {
+        if(selecting[i].classList.contains('selected')) {
+            content.appendChild(sections[i]);
+        }
+    }
+}
+
+showSection();
